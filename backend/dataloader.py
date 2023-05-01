e1f5d79820b094c950af34c8b7def7ba2f6504da
Didrik Munther <dmu0817@gmail.com>
2023-05-01 15:20:59 +0200

    Add boosts

--- backend/dataloader.py
+++ backend/dataloader.py
@@ -9,17 +9,17 @@ def indexTMDB(filename):
     for i,row in enumerate(reader):
         if i == 4553: continue
         movie = {
-            "budget" : row["budget"],
+            "budget" : float(row["budget"]),
             "genres" : [genre["name"] for genre in json.loads(row["genres"])],
             "keywords" : [keyword["name"] for keyword in json.loads(row["keywords"])],
             "original language" : row["original_language"],
             "title" : row["title"],
             "overview" : row["overview"],
-            "popularity" : row["popularity"],
+            "popularity" : float(row["popularity"]),
             "production companies" : [company["name"] for company in json.loads(row["production_companies"])],
             "production countries" : [country["name"] for country in json.loads(row["production_countries"])],
             "release date" : row["release_date"],
-            "revenue" : row["revenue"],
+            "revenue" : int(row["revenue"]),
             "runtime" : row["runtime"],
             "spoken languages" : [language["name"] for language in json.loads(row["spoken_languages"])],
             "status" : row["status"],
@@ -31,10 +31,15 @@ def indexTMDB(filename):
 
 if __name__ == "__main__":
     es = Elasticsearch("http://localhost:9200") # Create an elastic search instance
+
+    try:
+        es.indices.delete(index='movies') #remove the index
+    except:
+        pass
+
     es.indices.create(index = "movies") # Create the movie index that we will use
     indexTMDB("./data/movies.csv") # Insert the movies
     es.indices.refresh(index="movies")
     print("The Number of inserted movies is ",es.count(index="movies")['count']) 
     res = es.get(index="movies", id=1)
     print("the first document is ",res['_source'])
-    # es.indices.delete(index='movies') #remove the index
--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,77 @@
-import requests
-from flask import Flask
+from elasticsearch import Elasticsearch
+from flask import Flask, request
 import sys
-
-import dataloader
+import sqlite3
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 app = Flask(__name__)
+con = sqlite3.connect("db.db")
+es = Elasticsearch("http://elastic:9200")
+
+
+def init_db():
+    cur = con.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS user (
+            id integer primary key
+        )
+    """)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS keywords (
+            user_id integer,
+            keyword string,
+            count integer,
+            FOREIGN KEY(user_id) REFERENCES user(id)
+        );
+    """)
+
+
+@app.route("/log_user_movie")
+def log_user_click(movie_id):
+    eprint("Logging user movie", movie_id)
+
+
+def get_user_preferences(user_id):
+    return {
+        'nasa': 10,
+        'bears': 20
+    }
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
 
-@app.route("/init_index")
-def init_index():
-    eprint('Got request')
+@app.route("/search")
+def search():
+    try:
+        query = request.args.to_dict()['q']
+    except KeyError:
+        return {'error': 'Require query parameter \'q\''}
 
-    res = requests.put('http://es03:9200/documents', data={
-        "mappings": {
-            "properties": {
-                "content": {
-                    "type": "text"
-                }
-            }
-        }
+    preferences = get_user_preferences(0)
+
+    resp = es.search(index='movies', size=5, query={
+        'boosts': {
+            'keywords': [{
+                'type': 'value',
+                'value': v,
+                'operation': 'multiply',
+                'factor': preferences[v]
+            } for v in preferences]
+        },
+        "query": "nature"
+        # 'multi_match': {
+        #     'query': query,
+        #     'fuzziness': 2,
+        #     'fields': ['keywords', 'overview', 'production_companies']
+        # }
     })
-
-    eprint('Res here', res)
-
-    return {}
-
+    return resp.body
 
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True, host='0.0.0.0', port=3000)
